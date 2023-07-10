c9ddc7f136190470a7c8862a4a6ce97152678988
coreyo-git <corey.oconnor@live.com.au>
2023-07-10 15:05:16 +1000

    112 error handling negatives - Added root4 and tests 7 & 8

--- 112.PathSum.cs
+++ 112.PathSum.cs
@@ -35,9 +35,46 @@ public class PathSum
 		{
 			return false;
 		}
+		int? root_val = root.val;
+		int? sum = 0;
 
-		return true;
+		return dfs(root, ref sum, root_val, targetSum);
 	}
+
+	static bool dfs(TreeNode root, ref int? sum, int? root_val, int targetSum)
+	{
+
+
+		if (root == null)
+		{
+			return false;
+		}
+		if (targetSum == 3)
+			Console.WriteLine($"sum: {sum} targetSum: {targetSum}, adding val: {root.val}");
+
+		sum += root.val;
+
+		if (root.left == null && root.right == null)
+		{
+			if (sum == targetSum)
+			{
+				if (targetSum == 3)
+				{
+					Console.WriteLine($"True!! : sum: {sum} targetSum: {targetSum}, adding val: {root.val}\n");
+				}
+				return true;
+			}
+			else sum -= root.val;
+		}
+
+		if (dfs(root.left!, ref sum, root_val, targetSum)) return true;
+		if(targetSum == 3)
+			Console.WriteLine("");
+		if (dfs(root.right!, ref sum, root_val, targetSum)) return true;
+
+		return false;
+	}
+
 
 	public static void Main(string[] args)
 	{
@@ -61,7 +98,7 @@ public class PathSum
 		// (1 --> 2): The sum is 3.
 		// (1 --> 3): The sum is 4.
 		// There is no root-to-leaf path with sum = 5.
-		
+
 		int targetSum2 = 5;
 		TreeNode root2 = new TreeNode(1);
 		root2.left = new TreeNode(2);
@@ -70,17 +107,33 @@ public class PathSum
 		// Input: root = [], targetSum = 0
 		// Output: false
 		// Explanation: Since the tree is empty, there are no root-to-leaf paths.
-		
+
 		int targetSum3 = 0;
 		TreeNode root3 = new TreeNode(null);
+
+		// Input: root = [1,-2,-3,1,3,-2,null,-1], targetSum = 3
+		// Output: false
+
+		int targetSum4 = 3;
+		TreeNode root4 = new TreeNode(1);
+		root4.left = new TreeNode(-2);
+		root4.left.right = new TreeNode(3);
+		root4.left.left = new TreeNode(1);
+		root4.left.left.left = new TreeNode(-1);
+		root4.right = new TreeNode(-3);
+		root4.right.left = new TreeNode(-2);
+
 		Debug.Assert(HasPathSum(root1, targetSum1), $"Test 1 root1 Failed\nExpected: true\nActual: false");
 		Debug.Assert(!HasPathSum(root1, 0), $"Test 2 root1 Failed\nExpected: false\nActual: true");
 
-		Debug.Assert(HasPathSum(root2, targetSum2), $"Test 3 root2 Failed\nExpected: true\nActual: false");
-		Debug.Assert(!HasPathSum(root2, 0), $"Test 4 root2 Failed\nExpected: false\nActual: true");
+		Debug.Assert(!HasPathSum(root2, targetSum2), $"Test 3 root2 Failed\nExpected: true\nActual: false");
+		Debug.Assert(HasPathSum(root2, 4), $"Test 4 root2 Failed\nExpected: true\nActual: false");
 
 		Debug.Assert(!HasPathSum(root3, targetSum3), $"Test 5 root3 Failed\nExpected: false\nActual: true");
 		Debug.Assert(!HasPathSum(root3, 99), $"Test 6 root3 Failed\nExpected: false\nActual: true");
+
+		Debug.Assert(!HasPathSum(root4, targetSum4), $"Test 7 root4 Failed\nExpected: false\nActual: true");
+		Debug.Assert(!HasPathSum(root4, 99), $"Test 8 root4 Failed\nExpected: false\nActual: true");
 	}
 }
 
