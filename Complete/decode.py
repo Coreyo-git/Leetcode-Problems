c6d0a4a409a9f9eef89ba41dc5dd516d5b8f93a5
coreyo-git <corey.oconnor@live.com.au>
2023-07-21 12:27:35 +1000

    optimized decode.py, removed globals

--- Complete/decode.py
+++ Complete/decode.py
@@ -1,53 +1,37 @@
-# Global variables
-# Dictionary to store the decoded key-value pairs (number-word mapping)
-decode_dict = {}
-triangular_numbers = []  # List to store the triangular numbers used for decoding
-
 # Function to decode the message
-# Pass in a file location as the message_file parameter
-
 def decode(message_file):
     # Step 1: Read and process the message file to populate decode_dict
-    # Open the message_file in read mode
+    decode_dict = {}
     with open(message_file, 'r') as f:
+        # Split the line into sections based on space delimiter
         for line in f:
-            # Split the line into sections based on space delimiter
-            sections = line.strip().split(' ')
             # Extract the number and the word from the sections
-            num, word = sections
+            num, word = line.strip().split(' ')
             # Convert the number to an integer
             num = int(num)
             # Store the number-word pair in the decode_dict
             decode_dict[num] = word
             
- 	# Step 2: Sort the decode_dict by keys to get the numbers in ascending order
+    # Step 2: Sort the decode_dict by keys to get the numbers in ascending order
     sorted_dict = dict(sorted(decode_dict.items()))
     
-    # Step 3: Get the triangular numbers up to the number of items in sorted_dict
-    get_triangular_numbers(len(sorted_dict))
+    # Step 3: Get the triangular numbers that exist in the sorted_dict keys
+    triangular_numbers = get_triangular_numbers(sorted_dict.keys())
     
     # Step 4: Decode the message using the sorted_dict and the triangular numbers
-    message_decoded = ""
-    for num in triangular_numbers:
-        value = sorted_dict[num]
-        message_decoded += f"{value} "
+    message_decoded = [sorted_dict[num] for num in triangular_numbers]
+    
+    # Step 5: Return the decoded message as a space-separated string
+    return ' '.join(message_decoded)
 
-    # Step 5: Return the decoded message
-    return message_decoded
+# Function to get triangular numbers from the list of keys
+def get_triangular_numbers(keys):
+    max_key = max(keys)
+    return [(n * (n + 1)) // 2 for n in range(1, max_key + 1) if n * (n + 1) // 2 in keys]
 
-# Function to get triangular numbers up to the number max
-def get_triangular_numbers(max):
-    n = 1
-    # while is less than or equal to the max number:
-    while (n * (n + 1)) // 2 <= max:
-        # Append the triangular number
-        triangular_numbers.append((n * (n + 1)) // 2) 
-        n += 1 # Increment by 1
-    return
-
-# Call the decode function with the input file path
-decode('inputs.txt')
-
+# Call the decode function with the input file path and print the result
+decoded_message = decode('inputs.txt')
+print(decoded_message)
 
 # Prompt
 # I need a function called decode(message_file) that can read in an encoded message from a .txt file and return the decoded version as a string.
@@ -64,9 +48,6 @@ decode('inputs.txt')
 # 1
 # 2 3
 # 4 5 6
-# 7 8 9 10
-# 11 12 13 14
-# 15 16 17 18 19
 
 # The numbers at the end of each line (1, 3 and 6) correspond to the words that are part of the message. You should ignore all the other words. So for the example input file above, the message words are:
 # 1: I
