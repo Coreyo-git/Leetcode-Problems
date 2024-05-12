9d7af88cc46b08064a284ed44066a689d2137177
coreyo-git <corey.oconnor@live.com.au>
2024-05-12 12:01:01 +1000

    Ehance base 2215.FindTheDifference testing
    Created AreEqual function to compare the inner lists for equality
    Added base values to test it returns the correct value
    - both initial tests passed
    
    This was going to be relapced with a custom ListComparer function but thought it may be a bit niche to implemenet as a TestUtil.

--- 2215.FindTheDifferenceOfTwoArrays.cs
+++ 2215.FindTheDifferenceOfTwoArrays.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using static Leetcode.TestUtils;
 
 namespace Leetcode
@@ -8,9 +9,17 @@ namespace Leetcode
         // answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
         // answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
         // Note that the integers in the lists may be returned in any order.
-        public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+        public static IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
         {
             IList<IList<int>> result = new List<IList<int>>();
+
+			// Added test 1 expected output to test if AreEqual function returns true as intended
+			// result.Add(new List<int> { 1, 3 });
+            // result.Add(new List<int> { 4, 6 });
+
+			// Added test 1 non expected output to test AreEqual function returns false as intended
+			result.Add(new List<int> { 1, 2 });
+            result.Add(new List<int> { 4, 6 });
 
             return result;
         }
@@ -22,37 +31,52 @@ namespace Leetcode
             // Explanation:
             // For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
             // For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
-            IList<IList<int>> test1Input = new List<IList<int>>();
-            test1Input.Add(new List<int> { 1, 2, 3 });
-            test1Input.Add(new List<int> { 2, 4, 6 });
 
-            IList<IList<int>> test1Output = FindDifference(test1Input);
+            IList<IList<int>> test1Output = FindDifference(new int[] {1,2,3}, new int[] {2,4,6});
             IList<IList<int>> test1Expected = new List<IList<int>>();
 
             // Add the sublists to test1Expected
             test1Expected.Add(new List<int> { 1, 3 });
             test1Expected.Add(new List<int> { 4, 6 });
 
-            Debug.Assert(test1Expected.SequenceEqual(test1Output, new ListComparer<int>()), formatTestErrorMessage <IList<IList<int>>(1, test1Expected, test1Output));
+            Debug.Assert(AreEqual(test1Expected, test1Output), FormatTestErrorMessage(1, test1Expected, test1Output));
 
             // Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
             // Output: [[3],[]]
             // Explanation:
             // For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
             // Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
-            IList<IList<int>> test2Input = new List<IList<int>>();
-            test2Input.Add(new List<int> { 1, 2, 3, 3 });
-            test2Input.Add(new List<int> { 1, 1, 2, 2 });
 
-            IList<IList<int>> test2Output = FindDifference(test2Input);
-            IList<IList<int>> test2Expected = new List<IList<int>>();
+            // IList<IList<int>> test2Output = FindDifference(new int[] {1,2,3,3}, new int[] {1,1,2,2});
+            // IList<IList<int>> test2Expected = new List<IList<int>>();
 
-            // Add the sublists to test1Expected
-            test1Expected.Add(new List<int> { 3 });
-            test1Expected.Add(new List<int>());
+            // // Add the sublists to test1Expected
+            // test1Expected.Add(new List<int> { 3 });
+            // test1Expected.Add(new List<int>());
 
-            Debug.Assert(test2Expected.SequenceEqual(test2Output, new ListComparer<int>()), formatTestErrorMessage <IList<IList<int>>(2, test2Expected, test2Output));
+            // Debug.Assert(test2Expected.SequenceEqual(test2Output), FormatTestErrorMessage <IList<IList<int>>>(2, test2Expected, test2Output));
 
         }
+
+		// Checks if the inner Lists are equal to one another.
+		public static bool AreEqual(IList<IList<int>> list1, IList<IList<int>> list2)
+		{
+			// if list lengths are not not equal return false
+			if (list1.Count != list2.Count)
+			{
+				return false;
+			}
+
+			// loop through list in lists checking for non equality
+			for (int i = 0; i < list1.Count; i++)
+			{
+				if (!list1[i].SequenceEqual(list2[i]))
+				{
+					return false;
+				}
+			}
+			
+			return true;
+		}
     }
 }
