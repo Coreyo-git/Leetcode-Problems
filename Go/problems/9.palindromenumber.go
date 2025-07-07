2208d4db6b4a8f9bc4efee098903ff9fdcde9aa5
coreyo-git <corey.oconnor@live.com.au>
2025-07-08 06:44:31 +1000

    Go 9.Palindrome Number Complete

--- Go/problems/9.palindromenumber.go
+++ Go/problems/9.palindromenumber.go
@@ -1,10 +1,22 @@
 package problems
+import (
+	"strconv"
+)
 
 // Given an integer x, return true if x is a palindrome, and false otherwise.
 
 func IsPalindrome(x int) bool {
-	
-	return false
+	// convert to string
+	xString := strconv.Itoa(x)
+
+	// loop string with 2 iterators start / end and compare
+	for i, j := 0, len(xString) - 1; i < j; i, j = i+1, j-1 {
+		// if they aren't equal the string can't be reversed as a palindrome
+		if xString[i] != xString[j] {
+			return false
+		}
+	}
+	return true
 }
 
 // Constraints:
