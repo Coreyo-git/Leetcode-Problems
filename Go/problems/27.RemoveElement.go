5a4767c53c5ba2cd337c294621f81e8b6ca37e68
coreyo-git <corey.oconnor@live.com.au>
2025-07-10 14:11:26 +1000

    Go 27.RemoveElement Complete using two pointers

--- Go/problems/27.RemoveElement.go
+++ Go/problems/27.RemoveElement.go
@@ -1,7 +1,22 @@
 package problems
 
 func RemoveElement(nums []int, val int) (int, []int) {
-	total := 0
+	p1 := 0 // Read Pointer
+	p2 := 0 // Write Pointer
 
-	return total, nums
+	// loop read pointer through whole array
+	for p1 < len(nums) {
+		// if value at read pointer is not equal to the val continue
+		if nums[p1] != val {
+			// Move first good element to start of sorted section
+			nums[p2] = nums[p1]
+			// increment to next slot for non val ints
+			p2++
+		}
+
+		p1++
+	}
+	// return p2 because it's index the exact count of elements that were kept
+	// which is the next spot in the array for a good/ non val element
+	return p2, nums
 }
